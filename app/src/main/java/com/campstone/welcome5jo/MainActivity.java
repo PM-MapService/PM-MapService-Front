342a05ee7be7bf58fdf1499084520bb3cb3a6f89
prkim99@gmail.com <prkim99@gmail.com>
2022-11-03 22:40:57 +0900

    생성

---
+++ app/src/main/java/com/campstone/welcome5jo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.campstone.welcome5jo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.skt.Tmap.TMapMarkerItem;
+import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayoutTmap = (LinearLayout) findViewById(R.id.linearLayoutTmap);
+        TMapView tMapView = new TMapView(this);
+        TMapMarkerItem markerItem1 = new TMapMarkerItem();
+
+        TMapPoint tMapPoint1 = new TMapPoint(37.570841, 126.985302); // SKT타워
+
+// 마커 아이콘
+        Bitmap marker = BitmapFactory.decodeResource(getResources(), R.drawable.parking);
+        marker = Bitmap.createScaledBitmap(marker, 150, 150, true);
+        markerItem1.setIcon(marker); // 마커 아이콘 지정
+        markerItem1.setPosition(0.5f, 1.0f); // 마커의 중심점을 중앙, 하단으로 설정
+        markerItem1.setTMapPoint(tMapPoint1); // 마커의 좌표 지정
+        markerItem1.setName("SKT타워"); // 마커의 타이틀 지정
+        tMapView.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
+
+        tMapView.setCenterPoint(126.985302, 37.570841);
+
+        tMapView.setSKTMapApiKey("l7xxcf8d3af1899b4f168f7a593671f0c749");
+        linearLayoutTmap.addView(tMapView);
+        tMapView.setZoomLevel(18);
+    }
+}
