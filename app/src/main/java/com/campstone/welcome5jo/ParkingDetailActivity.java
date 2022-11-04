9bc213475eda6ec9398c878981619c2d94d433df
prkim99@gmail.com <prkim99@gmail.com>
2022-11-04 21:32:02 +0900

    주차구역 상세화면 frame

---
+++ app/src/main/java/com/campstone/welcome5jo/ParkingDetailActivity.java
@@ -0,0 +1,19 @@
+package com.campstone.welcome5jo;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.skt.Tmap.TMapMarkerItem;
+import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapView;
+
+public class ParkingDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.parking_detail);
+    }
+}
