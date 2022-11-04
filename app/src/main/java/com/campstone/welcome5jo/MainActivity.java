e774987b0dc3a7c01ee64080bd235e38f1c4f622
prkim99@gmail.com <prkim99@gmail.com>
2022-11-04 15:32:02 +0900

    홈화면

--- app/src/main/java/com/campstone/welcome5jo/MainActivity.java
+++ app/src/main/java/com/campstone/welcome5jo/MainActivity.java
@@ -1,9 +1,15 @@
 package com.campstone.welcome5jo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -20,26 +26,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RelativeLayout linearLayoutTmap = (RelativeLayout)  findViewById(R.id.linearLayoutTmap);
+        LinearLayout linearLayoutTmap = (LinearLayout) findViewById(R.id.linearLayoutTmap);
         TMapView tMapView = new TMapView(this);
+
+        TMapMarkerItem markerItem1=makeMarker(new TMapPoint(37.45074006,126.6567586)); // 하이테크)
+        tMapView.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
+
+        tMapView.setCenterPoint(126.6567586,37.45074006);
+        tMapView.setSKTMapApiKey("l7xxcf8d3af1899b4f168f7a593671f0c749");
+        linearLayoutTmap.addView(tMapView);
+        //지도 축척 조정
+        tMapView.setZoomLevel(17);
+    }
+
+    protected TMapMarkerItem makeMarker(TMapPoint tMapPoint){
         TMapMarkerItem markerItem1 = new TMapMarkerItem();
 
-        TMapPoint tMapPoint1 = new TMapPoint(37.570841, 126.985302); // SKT타워
-
-// 마커 아이콘
         Bitmap marker = BitmapFactory.decodeResource(getResources(), R.drawable.parking);
         marker = Bitmap.createScaledBitmap(marker, 150, 150, true);
         markerItem1.setIcon(marker); // 마커 아이콘 지정
         markerItem1.setPosition(0.5f, 1.0f); // 마커의 중심점을 중앙, 하단으로 설정
-        markerItem1.setTMapPoint(tMapPoint1); // 마커의 좌표 지정
-        markerItem1.setName("SKT타워"); // 마커의 타이틀 지정
-        tMapView.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
-
-        tMapView.setCenterPoint(126.985302, 37.570841);
-
-        tMapView.setSKTMapApiKey("l7xxcf8d3af1899b4f168f7a593671f0c749");
-        linearLayoutTmap.addView(tMapView);
-        //지도 축척 조정
-        tMapView.setZoomLevel(18);
+        markerItem1.setTMapPoint(tMapPoint); // 마커의 좌표 지정
+        markerItem1.setName("하이테크센터"); // 마커의 타이틀 지정
+        return markerItem1;
     }
 }
